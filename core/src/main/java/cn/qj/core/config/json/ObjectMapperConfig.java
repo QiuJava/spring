d722405f6115510fb0132dfc6743cd561eb10254
qiujian <qiujian@eeepay.cn>
2018-11-16 14:42:59 +0800

    mvc返回json数据null 为 ""

--- core/src/main/java/cn/qj/core/config/json/ObjectMapperConfig.java
+++ core/src/main/java/cn/qj/core/config/json/ObjectMapperConfig.java
@@ -1,11 +1,19 @@
 package cn.qj.core.config.json;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
 
 import cn.qj.core.util.DateUtil;
 
@@ -19,9 +27,18 @@ import cn.qj.core.util.DateUtil;
 public class ObjectMapperConfig {
 
 	@Bean
-	public ObjectMapper objectMapper() {
-		ObjectMapper objectMapper = new ObjectMapper();
+	@Primary
+	@ConditionalOnMissingBean(ObjectMapper.class)
+	public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
+		ObjectMapper objectMapper = builder.createXmlMapper(false).build();
 		objectMapper.setDateFormat(new SimpleDateFormat(DateUtil.DATE_TIME_FORMAT_PATTERN));
+		objectMapper.getSerializerProvider().setNullValueSerializer(new JsonSerializer<Object>() {
+			@Override
+			public void serialize(Object o, JsonGenerator jsonGenerator, SerializerProvider serializerProvider)
+					throws IOException, JsonProcessingException {
+				jsonGenerator.writeString("");
+			}
+		});
 		return objectMapper;
 	}
 }
