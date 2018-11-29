8ae4ae970994914c98cc52e66b723e288aa41204
qiujian <qiujian@eeepay.cn>
2018-11-29 15:14:03 +0800

    配置文件调整

--- core/src/main/java/cn/qj/core/mapper/IpLogMapper.java
+++ core/src/main/java/cn/qj/core/mapper/IpLogMapper.java
@@ -2,16 +2,58 @@ package cn.qj.core.mapper;
 
 import cn.qj.core.entity.IpLog;
 
+/**
+ * 登录日志数据操作
+ * 
+ * @author Qiujian
+ * @date 2018/11/29
+ */
 public interface IpLogMapper {
-    int deleteByPrimaryKey(Long id);
+	/**
+	 * 根据主键删除
+	 * 
+	 * @param id
+	 * @return
+	 */
+	int deleteByPrimaryKey(Long id);
 
-    int insert(IpLog record);
+	/**
+	 * 插入一条记
+	 * 
+	 * @param record
+	 * @return
+	 */
+	int insert(IpLog record);
 
-    int insertSelective(IpLog record);
+	/**
+	 * 插入部分列的记录
+	 * 
+	 * @param record
+	 * @return
+	 */
+	int insertSelective(IpLog record);
 
-    IpLog selectByPrimaryKey(Long id);
+	/**
+	 * 根据主键查询一条记录
+	 * 
+	 * @param id
+	 * @return
+	 */
+	IpLog selectByPrimaryKey(Long id);
 
-    int updateByPrimaryKeySelective(IpLog record);
+	/**
+	 * 根据主键更新不部分列
+	 * 
+	 * @param record
+	 * @return
+	 */
+	int updateByPrimaryKeySelective(IpLog record);
 
-    int updateByPrimaryKey(IpLog record);
+	/**
+	 * 根据主键更新
+	 * 
+	 * @param record
+	 * @return
+	 */
+	int updateByPrimaryKey(IpLog record);
 }
