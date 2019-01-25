650d6ad973340e57dd2dc363152de128f3a307de
qiujian <qiujian@eeepay.cn>
2019-01-25 15:39:32 +0800

    去除依赖危险

--- key_app/src/test/java/cn/qj/key/KeyAppApplicationTest.java
+++ key_app/src/test/java/cn/qj/key/KeyAppApplicationTest.java
@@ -2,8 +2,11 @@ package cn.qj.key;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import cn.qj.key.util.WechatUtils;
 
 /**
  * 测试
@@ -14,9 +17,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class KeyAppApplicationTest {
+	
+	@Autowired
+	private WechatUtils util;
 
 	@Test
 	public void contextLoads() {
+		System.out.println(util.getAppId());
+		System.out.println(util.getAppsecret());
 	}
 
 }
