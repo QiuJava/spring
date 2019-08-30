b6ab6ae0514cb87e5bd9025f6db854e6bef1b942
qiujian <qiujian@eeepay.cn>
2019-08-30 13:53:52 +0800

    消除项目 xml文件

--- loan/src/main/java/cn/loan/LoanApplication.java
+++ loan/src/main/java/cn/loan/LoanApplication.java
@@ -2,8 +2,6 @@ package cn.loan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.context.annotation.Profile;
 
 /**
  * 应用启动
@@ -12,8 +10,6 @@ import org.springframework.context.annotation.Profile;
  *
  */
 @SpringBootApplication
-@ServletComponentScan
-@Profile("dev")
 public class LoanApplication {
 
 	public static void main(String[] args) {
