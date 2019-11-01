70b9e7dd87d26060295eb8dd650a11c97177d8ef
qiujian <qiujian@eeepay.cn>
2019-11-01 16:18:50 +0800

    添加工具类

--- mybatis/src/main/java/com/example/util/DateTimeUtil.java
+++ mybatis/src/main/java/com/example/util/DateTimeUtil.java
@@ -1,5 +1,9 @@
 package com.example.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 /**
  * 日期时间工具
  * 
@@ -12,4 +16,44 @@ public class DateTimeUtil {
 	public static final String DATATIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
 	public static final String DATA_PATTERN = "yyyy-MM-dd";
 	public static final String MONTH_PATTERN = "yyyyMM";
+
+	/**
+	 * 获取当前时间
+	 * 
+	 * @return new Date()
+	 */
+	public static Date getNewDate() {
+		return new Date();
+	}
+
+	/**
+	 * 日期对象转换为 日期时间字符串
+	 * 
+	 * @return 1970-1-1 00:00:00
+	 */
+	public static String getDateTimeStr(Date date) {
+		SimpleDateFormat format = new SimpleDateFormat(DATATIME_PATTERN, Locale.CHINA);
+		return format.format(date);
+	}
+
+	/**
+	 * 日期对象转换为日期字符串
+	 * 
+	 * @return 1970-1-1
+	 */
+	public static String getDateStr(Date date) {
+		SimpleDateFormat format = new SimpleDateFormat(DATA_PATTERN, Locale.CHINA);
+		return format.format(date);
+	}
+
+	/**
+	 * 日期对象转换为日期字符串
+	 * 
+	 * @return 197001
+	 */
+	public static String getYearMonthStr(Date date) {
+		SimpleDateFormat format = new SimpleDateFormat(MONTH_PATTERN, Locale.CHINA);
+		return format.format(date);
+	}
+
 }
--- mybatis/src/main/java/com/example/util/StrUtil.java
+++ mybatis/src/main/java/com/example/util/StrUtil.java
@@ -1,5 +1,8 @@
 package com.example.util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * 字符串工具
  *
@@ -8,6 +11,42 @@ package com.example.util;
  */
 public class StrUtil {
 	private StrUtil() {
+
+	}
+
+	/**
+	 * 空字符串
+	 */
+	public static final String EMPTY = "";
+
+	/**
+	 * 手机号码正则
+	 */
+	public static final String PHONE_PATTERN = "^((13[0-9])|(15[^4])|(18[0,2,3,5-9])|(17[0-8])|(147))\\d{8}$";
+	/**
+	 * 数字正则
+	 */
+	public static final String NUM_PATTERN = "[0-9]*";
+	/**
+	 * 中文正则
+	 */
+	public static final String ZW_PATTERN = "[\\u4e00-\\u9fa5]*";
+	/**
+	 * 字母正则
+	 */
+	public static final String ZM_PATTERN = "^[A-Za-z]+$";
+
+	/**
+	 * 是否匹配
+	 * 
+	 * @param str     字符串
+	 * @param pattern 正则表达式
+	 * @return
+	 */
+	public static boolean isPattern(String str, String pattern) {
+		Pattern p = Pattern.compile(pattern);
+		Matcher m = p.matcher(str);
+		return m.matches();
 	}
 
 	/**
