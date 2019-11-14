bd5f097796eb466f489bb2a77a14e739d9970d51
qiujian <qiujian@eeepay.cn>
2019-11-14 13:54:35 +0800

    任务查询对象更新

--- mybatis/src/main/java/com/example/qo/JobDetailsQo.java
+++ mybatis/src/main/java/com/example/qo/JobDetailsQo.java
@@ -1,5 +1,7 @@
 package com.example.qo;
 
+import com.example.common.Result;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -13,8 +15,19 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-public class JobDetailsQo extends BaseQo {
+public class JobDetailsQo {
 
 	private String jobGroupName;
+	private Integer pageNum;
+	private Integer pageSize;
 
+	public Result verify() {
+		if (this.pageNum == null || pageNum < 1) {
+			return new Result(false, "页数不正确");
+		}
+		if (this.pageSize == null || pageSize < 0) {
+			return new Result(false, "一页条数不正确");
+		}
+		return null;
+	}
 }
